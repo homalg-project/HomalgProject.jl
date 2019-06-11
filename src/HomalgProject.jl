8f4bc87d0ce059ca2f18275c8f416a546c9b7943
Mohamed Barakat <mohamed.barakat@uni-siegen.de>
2019-06-11 09:33:17 +0200

    exported @g_str, @gap
    thanks @sebasguts

--- src/HomalgProject.jl
+++ src/HomalgProject.jl
@@ -7,8 +7,8 @@ import Base: getindex
 import GAP
 export GAP
 
-import GAP: LoadPackageAndExposeGlobals
-export LoadPackageAndExposeGlobals
+import GAP: LoadPackageAndExposeGlobals, @g_str, @gap
+export LoadPackageAndExposeGlobals, @g_str, @gap
 
 Base.:*(x::GAP.MPtr,y::String) = x*GAP.julia_to_gap(y)
 Base.getindex(x::GAP.MPtr,y::String) = GAP.Globals.ELM_LIST(x,GAP.julia_to_gap(y))
