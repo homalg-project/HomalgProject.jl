3315bb89088f962d249b293c220901b1d566026c
Mohamed Barakat <mohamed.barakat@uni-siegen.de>
2019-06-20 21:54:02 +0200

    installed method for HomalgMatrix
    HomalgMatrix( M::String, m::Int64, n::Int64, R::GAP.GapObj )

--- src/HomalgProject.jl
+++ src/HomalgProject.jl
@@ -13,4 +13,10 @@ export LoadPackageAndExposeGlobals, @g_str, @gap
 Base.:*(x::GAP.GapObj,y::String) = x*GAP.julia_to_gap(y)
 Base.getindex(x::GAP.GapObj,y::String) = GAP.Globals.ELM_LIST(x,GAP.julia_to_gap(y))
 
+function HomalgMatrix( M::String, m::Int64, n::Int64, R::GAP.GapObj )
+    return GAP.Globals.HomalgMatrix( GAP.julia_to_gap( M ), m, n, R )
+end
+
+export HomalgMatrix
+
 end # module
