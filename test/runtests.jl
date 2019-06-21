f0c85c0cf93642435f303972cf66ee1f993832c1
Mohamed Barakat <mohamed.barakat@uni-siegen.de>
2019-06-21 18:24:25 +0200

    fixed test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,18 @@ using Test
     LoadPackageAndExposeGlobals( "GradedModules", Main, all_globals = true )
     ℚ = HomalgFieldOfRationalsInSingular( )
     R = ℚ["x,y,z"]
-    M = "[ x*y,y*z,z,0,0, x^3*z,x^2*z^2,0,x*z^2,-z^2, x^4,x^3*z,0,x^2*z,-x*z, 0,0,x*y,-y^2,x^2-1, 0,0,x^2*z,-x*y*z,y*z, 0,0,x^2*y-x^2,-x*y^2+x*y,y^2-y]";
-    M = HomalgMatrix( GAP.julia_to_gap( M ), 6, 5, R )
-    @test NrRows( M ) == 6
-    @test NrColumns( M ) == 5
-    M = LeftPresentation( M )
+    m = "[ x*y,y*z,z,0,0, x^3*z,x^2*z^2,0,x*z^2,-z^2, x^4,x^3*z,0,x^2*z,-x*z, 0,0,x*y,-y^2,x^2-1, 0,0,x^2*z,-x*y*z,y*z, 0,0,x^2*y-x^2,-x*y^2+x*y,y^2-y]";
+    m = HomalgMatrix( m, 6, 5, R )
+    @test NrRows( m ) == 6
+    @test NrColumns( m ) == 5
+    M = LeftPresentation( m )
     @test NrRelations( M ) == 6
     @test NrGenerators( M ) == 5
-    SetAsOriginalPresentation( M )
     FilteredByPurity( M )
     @test NrRelations( M ) == 12
     @test NrGenerators( M ) == 9
     @test RankOfObject( M ) == 2
-    OnOriginalPresentation( M )
+    OnFirstStoredPresentation( M )
     @test NrRelations( M ) == 6
     @test NrGenerators( M ) == 5
     OnLastStoredPresentation( M )
