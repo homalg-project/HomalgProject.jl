b2860b7650177a1a0416c30221e1d1040efa4463
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2020-03-14 16:41:30 +0100

    relocated lines

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,14 @@ using Documenter, HomalgProject
 makedocs(;
     modules  = [HomalgProject],
     format   = Documenter.HTML(assets=String[]),
-    pages    = [
-        "Home" => "index.md",
-        "Examples" => "examples.md",
-    ],
     doctest  = false,
     repo     = "https://github.com/homalg-project/HomalgProject.jl/blob/{commit}{path}#L{line}",
     sitename = "HomalgProject.jl",
     authors  = "Mohamed Barakat <mohamed.barakat@uni-siegen.de>",
+    pages    = [
+        "Home" => "index.md",
+        "Examples" => "examples.md",
+    ],
 )
 
 deploydocs(;
